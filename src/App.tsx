c3d72b47fa59600433f9a0825db2fdcf22cdeaf0
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-08-24 11:38:52 +0200

    Better icons + refacto css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import classNames from "classnames";
 import { ReactNode, useEffect, useState } from "react";
-import { IconType } from "react-icons";
-import { AiOutlinePlus } from "react-icons/ai";
 import {
-  CiBrightnessUp,
-  CiInboxIn,
-  CiSquareChevLeft,
-  CiSquareChevRight,
-} from "react-icons/ci";
-import { FiCheck } from "react-icons/fi";
-import { PiPlusLight } from "react-icons/pi";
+  IconBug,
+  IconBurger,
+  IconCalendar,
+  IconCheck,
+  IconChevronLeft,
+  IconChevronRight,
+  IconInbox,
+  IconPlus,
+} from "./Icons";
 
 export default function App() {
   const [sidebarOpened, setSidebarOpened] = useState(false);
@@ -24,67 +24,27 @@ export default function App() {
 
   return (
     <div className="inter">
-      <nav className="fixed bottom-0 z-20 flex flex-row items-center justify-between w-screen h-16 gap-8 px-4 text-black bg-gray-100 sm:hidden">
-        <button
-          aria-label="toggle sidebar"
-          id="openSideBar"
-          className="sm:hidden"
-          onClick={() => {
-            setSidebarOpened(!sidebarOpened);
-          }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="w-6 h-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-        </button>
-
-        <NavContent />
+      <nav className="fixed bottom-0 z-20 w-screen h-16 text-black bg-gray-100 sm:hidden">
+        <NavContent
+          openSidebarHandler={() => setSidebarOpened(!sidebarOpened)}
+        />
       </nav>
 
-      <div>
-        {/* Transparent black background to close sidebar */}
-        <button
-          aria-label="toggle sidebar"
-          id="openSideBar"
-          hidden={!sidebarOpened}
-          className={classNames(
-            "fixed left-0 z-30 backdrop-blur-md w-screen h-screen bg-black sm:hidden bg-opacity-30 transition duration-300 ease-in-out"
-          )}
-          onClick={() => {
-            setSidebarOpened(!sidebarOpened);
-          }}
-        ></button>
-
-        <div
-          className={classNames(
-            "fixed flex-col w-64 sm:w-48 md:w-64 bg-gray-100 shadow h-screen flex z-40 transition duration-150 ease-in-out sm:transform-none overflow-scroll px-4 py-4",
-            {
-              "translate-x-0": sidebarOpened,
-              "-translate-x-64": !sidebarOpened,
-            }
-          )}
-        >
-          <SidemenuContent />
-        </div>
+      <div
+        className={classNames(
+          "fixed flex-col w-full sm:w-48 md:w-64 bg-white sm:bg-gray-100 h-[calc(100vh-4rem)] sm:h-screen flex z-40  overflow-scroll px-4 py-4 sm:transform-none",
+          {
+            "translate-x-0": sidebarOpened,
+            "-translate-x-full": !sidebarOpened,
+          }
+        )}
+      >
+        <SidemenuContent />
       </div>
 
       <div className="pb-16 sm:pb-0 w-full sm:w-[calc(100vw-12rem)] md:w-[calc(100vw-16rem)] sm:ml-48 md:ml-64">
         <div className="container max-w-3xl px-3 py-4 mx-auto ">
           <TodayPage />
-          {/* <div className="w-full h-[200vh] border-2 border-gray-300 border-dashed rounded">
-            aaa
-          </div> */}
         </div>
       </div>
     </div>
@@ -103,9 +63,9 @@ function TodayPage() {
 
   return (
     <>
-      <div className="flex flex-row items-center justify-center gap-3 sm:gap-4 group">
-        <button>
-          <CiSquareChevLeft className="w-6 h-6 text-gray-500 transition duration-150 ease-in-out no-touch:opacity-0 group-hover:opacity-100 hover:text-black" />
+      <div className="flex flex-row items-center justify-center max-w-xl gap-3 mx-auto sm:gap-4 group">
+        <button className="text-gray-500 animated no-touch:opacity-0 group-hover:opacity-100 hover:text-black">
+          <IconChevronLeft />
         </button>
         <CalendarDayItem tasksDone={1} tasksTotal={3} date={dayAdd(-4)} />
         <CalendarDayItem tasksDone={1} tasksTotal={1} date={dayAdd(-3)} />
@@ -119,13 +79,15 @@ function TodayPage() {
         />
         <CalendarDayItem tasksDone={1} tasksTotal={1} date={dayAdd(1)} />
         <CalendarDayItem tasksDone={1} tasksTotal={7} date={dayAdd(2)} />
-        <button>
-          <CiSquareChevRight className="w-6 h-6 text-gray-500 transition duration-150 ease-in-out no-touch:opacity-0 group-hover:opacity-100 hover:text-black" />
+        <button className="text-gray-500 animated no-touch:opacity-0 group-hover:opacity-100 hover:text-black">
+          <IconChevronRight />
         </button>
       </div>
 
-      <div className="flex flex-row items-center w-full p-2 my-4 bg-gray-50 border border-gray-50 rounded-lg hover:border-gray-300 focus-within:!border-gray-500 focus-within:bg-gray-100">
-        <PiPlusLight className="w-8 h-8 text-gray-500" />
+      <div className="flex flex-row items-center w-full my-4 button bg-gray-50 focus-within:bg-gray-100 ">
+        <span className="text-gray-500">
+          <IconPlus />
+        </span>
 
         <input
           id="task-creation-field"
@@ -192,14 +154,14 @@ function Task({
 
       <div className="flex flex-row flex-wrap sm:flex-nowrap items-center justify-between sm:justify-normal w-[calc(100%-1.5rem-0.5rem)]">
         <div className="flex-shrink-0 text-xs font-light text-right sm:text-sm sm:basis-24 sm:text-center">
-          <button className="p-1 border border-transparent rounded-lg hover:border-gray-300">
+          <button className="button">
             Saturday <br className="hidden sm:inline" />
             14 august
           </button>
         </div>
 
         <div className="flex-shrink-0 sm:w-24">
-          <button className="flex flex-row items-center gap-1 p-1 text-xs font-light border border-transparent rounded-lg hover:border-gray-300 sm:text-sm">
+          <button className="flex flex-row items-center gap-1 text-xs font-light sm:text-sm button">
             <span>{projectEmoji}</span>
             <span className="overflow-auto break-words hyphens-auto">
               {projectName}
@@ -212,7 +174,7 @@ function Task({
 
         <div
           contentEditable
-          className="p-1 overflow-auto break-words border border-transparent rounded-lg grow hyphens-auto hover:border-gray-300 focus:border-gray-500 without-ring"
+          className="overflow-auto break-words button grow hyphens-auto without-ring"
         >
           {title}
         </div>
@@ -254,26 +216,26 @@ function CalendarDayItem({
 
   function DateDisplay() {
     return (
-      <div className="flex flex-col items-center w-5 h-16 md:w-8">
+      <div className="flex flex-col items-center h-16">
         <div className="text-xs">{days[date.getDay()]}</div>
 
         <div>{date.getDate()}</div>
 
         <div
           className={classNames(
-            "flex items-center justify-center w-3 h-3 text-xs text-white bg-black rounded-full",
+            "flex items-center justify-center w-4 h-4 p-[2px] text-xs text-white bg-black rounded-full",
             {
               invisible: false,
               "bg-green-600": beforeToday || isToday,
-              "bg-transparent": beforeToday && tasksDone != tasksTotal,
+              "bg-violet-600": beforeToday && tasksDone != tasksTotal,
             }
           )}
         >
           {beforeToday ? (
             tasksDone == tasksTotal ? (
-              <FiCheck />
+              <IconCheck />
             ) : (
-              "👻"
+              <IconBug />
             )
           ) : tasksPending < 10 ? (
             tasksPending
@@ -288,9 +250,10 @@ function CalendarDayItem({
   return (
     <button
       className={classNames(
-        "flex flex-row items-center border-b border-black hover:opacity-100 transition duration-150 ease-in-out",
+        "flex flex-row grow items-center justify-center border-b border-black hover:opacity-100 animated",
         {
           "opacity-50": !isFocused,
+          "w-18": isToday,
         }
       )}
     >
@@ -304,30 +267,32 @@ function CalendarDayItem({
   );
 }
 
-function NavContent() {
+function NavContent({
+  openSidebarHandler,
+}: {
+  openSidebarHandler: () => void;
+}) {
   function handleCreateTask() {
     document.getElementById("task-creation-field")?.focus();
   }
 
   return (
-    <>
-      <button className="flex items-center justify-center w-20 h-full">
-        <CiInboxIn className="w-8 h-8 text-blue-600" />
-      </button>
-
+    <div className="flex flex-row items-center justify-around h-full">
       <button
-        className="flex items-center justify-center w-20 h-full"
-        onClick={handleCreateTask}
+        className="flex items-center justify-center w-20 h-full text-gray-400"
+        onClick={openSidebarHandler}
       >
-        <AiOutlinePlus className="w-8 h-8 text-red-600" />
+        <IconBurger />
       </button>
 
-      <button className="flex items-center justify-center w-20 h-full bg-gray-200">
-        <CiBrightnessUp className="w-8 h-8 text-yellow-600" />
+      <button className="flex items-center justify-center w-20 h-full text-gray-600 border-t border-gray-600">
+        <IconCalendar />
       </button>
 
-      <button className="invisible w-10 h-10"></button>
-    </>
+      <button className="flex items-center justify-center w-20 h-full text-gray-400">
+        <IconInbox />
+      </button>
+    </div>
   );
 }
 
@@ -345,16 +310,17 @@ function SidemenuContent() {
 
       <SidemenuItem
         className="hidden sm:list-item"
-        Icon={CiInboxIn}
+        Icon={IconInbox}
         iconColor="text-blue-600"
       >
         Inbox
       </SidemenuItem>
       <SidemenuItem
         className="hidden sm:list-item"
-        Icon={CiBrightnessUp}
-        iconColor="text-yellow-600"
+        Icon={IconCalendar}
+        iconColor="text-green-600"
         chip="5"
+        active
       >
         Today
       </SidemenuItem>
@@ -376,25 +342,31 @@ function SidemenuItem({
   emoji = "",
   chip = "",
   className = "",
+  active,
 }: {
   children: ReactNode;
-  Icon?: IconType;
+  Icon?: any;
   iconColor?: string;
   emoji?: string;
   chip?: string;
   className?: string;
+  active?: boolean;
 }) {
   return (
     <li className={className}>
       <a
         href="#"
-        className="flex items-center px-2 py-1 mb-1 rounded-md hover:bg-gray-200 group"
+        className={classNames("flex items-center mb-1 sidemenu-button group", {
+          "sidemenu-button-active": active,
+        })}
       >
         {Icon ? (
-          <Icon className={classNames("flex-shrink-0 w-5 h-5", iconColor)} />
+          <div className={classNames("flex-shrink-0", iconColor)}>
+            <Icon mini />
+          </div>
         ) : (
           /* If no emoji, still keep element to preserve spacing */
-          <div className="flex-shrink-0 w-5 text-center">{emoji}</div>
+          <div className="flex-shrink-0 w-4 text-center">{emoji}</div>
         )}
 
         <span className="flex-1 ml-3 font-light text-gray-800 whitespace-nowrap">
@@ -402,7 +374,7 @@ function SidemenuItem({
         </span>
 
         {chip != "" ? (
-          <span className="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-xs text-black rounded-full bg-zinc-300">
+          <span className="inline-flex items-center justify-center w-5 h-5 ml-3 text-xs text-black rounded-full bg-zinc-300">
             {chip}
           </span>
         ) : (
---
+++ src/Icons.tsx
@@ -0,0 +1,153 @@
+// Icons from Heroicons
+
+import classNames from "classnames";
+
+export function IconCalendar({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
+      />
+    </svg>
+  );
+}
+
+export function IconBurger() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+      />
+    </svg>
+  );
+}
+
+export function IconInbox({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 3.75H6.912a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H15M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859M12 3v8.25m0 0l-3-3m3 3l3-3"
+      />
+    </svg>
+  );
+}
+
+export function IconChevronLeft({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.75 19.5L8.25 12l7.5-7.5"
+      />
+    </svg>
+  );
+}
+
+export function IconChevronRight({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+      />
+    </svg>
+  );
+}
+
+export function IconPlus({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 4.5v15m7.5-7.5h-15"
+      />
+    </svg>
+  );
+}
+
+export function IconCheck({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className={classNames({ "w-6 h-6": !mini, "w-4 h-4": mini })}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M4.5 12.75l6 6 9-13.5"
+      />
+    </svg>
+  );
+}
+
+export function IconBug({ mini }: { mini?: boolean }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      fill="currentColor"
+      className="w-5 h-5"
+    >
+      <path
+        fillRule="evenodd"
+        d="M6.56 1.14a.75.75 0 01.177 1.045 3.989 3.989 0 00-.464.86c.185.17.382.329.59.473A3.993 3.993 0 0110 2c1.272 0 2.405.594 3.137 1.518.208-.144.405-.302.59-.473a3.989 3.989 0 00-.464-.86.75.75 0 011.222-.869c.369.519.65 1.105.822 1.736a.75.75 0 01-.174.707 7.03 7.03 0 01-1.299 1.098A4 4 0 0114 6c0 .52-.301.963-.723 1.187a6.961 6.961 0 01-1.158.486c.13.208.231.436.296.679 1.413-.174 2.779-.5 4.081-.96a19.655 19.655 0 00-.09-2.319.75.75 0 111.493-.146 21.239 21.239 0 01.08 3.028.75.75 0 01-.482.667 20.874 20.874 0 01-5.153 1.249 2.51 2.51 0 01-.107.247 20.86 20.86 0 015.253 1.257.75.75 0 01.48.74 20.946 20.946 0 01-.907 5.107.75.75 0 01-1.433-.444c.415-1.34.69-2.743.806-4.19-.495-.174-1-.328-1.512-.461.05.284.076.575.076.873 0 1.814-.517 3.312-1.426 4.37A4.639 4.639 0 0110 19a4.64 4.64 0 01-3.574-1.63C5.516 16.311 5 14.813 5 13c0-.298.026-.59.076-.873-.513.133-1.017.287-1.512.46.116 1.448.39 2.85.806 4.191a.75.75 0 01-1.433.444 20.94 20.94 0 01-.908-5.107.75.75 0 01.482-.74 20.857 20.857 0 015.252-1.257 2.481 2.481 0 01-.107-.247 20.874 20.874 0 01-5.153-1.249.75.75 0 01-.482-.667 21.342 21.342 0 01.08-3.028.75.75 0 111.493.146 19.745 19.745 0 00-.09 2.32c1.302.459 2.668.785 4.08.959.066-.243.166-.471.297-.679a6.962 6.962 0 01-1.158-.486A1.348 1.348 0 016 6a4 4 0 01.166-1.143 7.032 7.032 0 01-1.3-1.098.75.75 0 01-.173-.707 5.48 5.48 0 01.822-1.736.75.75 0 011.046-.176z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
