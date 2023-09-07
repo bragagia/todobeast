8a2e8c4ea62e3da13fdb71419efd9e7bf9d58684
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-07 22:44:14 +0200

    Add a replicache update when tab is visible again

--- src/spa/ReplicacheProvider.tsx
+++ src/spa/ReplicacheProvider.tsx
@@ -94,21 +94,37 @@ export function ReplicacheProvider({ children }: { children: ReactNode }) {
 // Implements a Replicache poke using Supabase's realtime functionality.
 // See: backend/poke/supabase.ts.
 function listen(supabase: SupabaseClient, onPoke: () => Promise<void>) {
-  let channel = supabase
-    .channel("any")
-    .on(
-      "postgres_changes",
-      {
-        event: "*",
-        schema: "public",
-        table: "replicache_space",
-      },
-      () => {
-        onPoke();
-      }
-    )
-    .subscribe();
+  function createChannel() {
+    return supabase
+      .channel("any")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "replicache_space",
+        },
+        () => {
+          onPoke();
+        }
+      )
+      .subscribe();
+  }
+
+  let channel = createChannel();
+
+  async function handleVisibilityChange() {
+    if (!document.hidden) {
+      // Tab is in view again, restart listener just in case, and poke to be sure we didn't miss something.
+      await supabase.removeChannel(channel);
+      channel = createChannel();
+      onPoke();
+    }
+  }
+  document.addEventListener("visibilitychange", handleVisibilityChange);
+
   return () => {
     supabase.removeChannel(channel);
+    document.removeEventListener("visibilitychange", handleVisibilityChange);
   };
 }
