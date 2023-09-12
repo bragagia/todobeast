08eb113352ce335feb7fbe15c4ddb861d39908f1
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-12 17:21:54 +0200

    Tasks grouping and ordering, but clean

--- src/spa/App/Components/TaskList.tsx
+++ src/spa/App/Components/TaskList.tsx
@@ -12,7 +12,7 @@ import { useSubscribe } from "replicache-react";
 import { ProjectType, getAllProjects } from "../../../db/projects";
 import { PriorityType, TaskType } from "../../../db/tasks";
 import { useReplicache } from "../../ReplicacheProvider";
-import { calcNewOrder } from "../../utils/Orderring";
+import { dragAndDropGeneric as dragAndDropMultidropableGeneric } from "../../utils/Orderring";
 import { ProjectName } from "./ProjectName";
 import { Task } from "./Task";
 
@@ -29,15 +29,15 @@ export function TaskList({ tasks: uncachedTasks }: { tasks: TaskType[] }) {
 
     let allProjectsById: { [key: string]: ProjectType } = {};
 
-    for (let i = 0; i < allProjects.length; i++) {
-      allProjectsById[allProjects[i].id] = allProjects[i];
-    }
+    allProjects.forEach((project) => {
+      allProjectsById[project.id] = project;
+    });
 
     return allProjectsById;
   }, [allProjects]);
 
   const sortedTasks = useMemo(() => {
-    if (!allProjectsById) return null;
+    if (!allProjectsById || !tasks) return null;
 
     return tasks.sort((a, b) => {
       if (!a.done_at && b.done_at) return -1;
@@ -88,134 +88,65 @@ export function TaskList({ tasks: uncachedTasks }: { tasks: TaskType[] }) {
 
   const onDragTask = useCallback(
     ({ source, destination }: DropResult, provided: ResponderProvided) => {
-      console.log(source, destination);
       // dropped outside the list or at the same position
-      if (
-        !sortedTasksPerProjectAndDone ||
-        !allProjectsById ||
-        !destination ||
-        (destination.droppableId == source.droppableId &&
-          destination.index == source.index)
-      ) {
+      if (!sortedTasksPerProjectAndDone) {
         return;
       }
-      console.log("dragging");
 
-      // note: in this context: id = database id, index = draggable area index position
+      dragAndDropMultidropableGeneric(
+        { source, destination },
+        sortedTasksPerProjectAndDone,
+        {
+          projectId: (task) => task.projectId,
+          priority: (task) => task.priority,
+        },
+        (source, destination, sourceItem, newOrder, newSubvalues) => {
+          // We create the task update instruction with only the required valeus
+          let taskUpdate: Required<Pick<TaskType, "id">> & Partial<TaskType> = {
+            id: sourceItem.id,
+            order: newOrder,
+          };
 
-      let sourceTask =
-        sortedTasksPerProjectAndDone[source.droppableId][source.index];
+          // If task is moving from not done to the done bucket, we don't update project and priority (but we do if moving from done to done)
+          if (
+            source.droppableId !== "done" &&
+            destination.droppableId === "done"
+          ) {
+            taskUpdate = { ...taskUpdate, done_at: dayjs().toString() };
+          } else if (
+            source.droppableId === "done" &&
+            destination.droppableId !== "done"
+          ) {
+            taskUpdate = { ...taskUpdate, done_at: null };
+          }
 
-      let destinationProjectSortedTasks =
-        sortedTasksPerProjectAndDone[destination.droppableId];
-      // If we move a task to the last position of a perProjectAndDone, the destination task will not exist, so we use the last task as a reference
-      let destinationIsEndOfDraggable =
-        destination.index >=
-        sortedTasksPerProjectAndDone[destination.droppableId].length;
+          // We do not want to change projectId and priority if moving inside the done category
+          if (destination.droppableId !== "done") {
+            if (newSubvalues["projectId"] !== sourceItem.projectId) {
+              taskUpdate = {
+                ...taskUpdate,
+                projectId: newSubvalues["projectId"],
+              };
+            }
 
-      let destinationTask = destinationIsEndOfDraggable
-        ? sortedTasksPerProjectAndDone[destination.droppableId][
-            sortedTasksPerProjectAndDone[destination.droppableId].length - 1
-          ]
-        : sortedTasksPerProjectAndDone[destination.droppableId][
-            destination.index
-          ];
+            if (newSubvalues["priority"] !== sourceItem.priority) {
+              taskUpdate = {
+                ...taskUpdate,
+                priority: newSubvalues["priority"],
+              };
+            }
+          }
 
-      console.log("source and dest task: ", sourceTask, destinationTask);
+          // We update the task in cache in order to prevent blinking during the time when replicache update its state
+          setTasks(
+            tasks.map((task) =>
+              task.id === sourceItem.id ? { ...task, ...taskUpdate } : task
+            )
+          );
 
-      let sourceProjectAndDoneIndex = Object.keys(allProjectsById).findIndex(
-        (key) => key === source.droppableId
+          rep.mutate.taskUpdate(taskUpdate);
+        }
       );
-      let destinationProjectAndDoneIndex = Object.keys(
-        allProjectsById
-      ).findIndex((key) => key === destination.droppableId);
-
-      // TODO: ...AndInSameDraggable
-      let sourceIsBeforeDestination =
-        sourceProjectAndDoneIndex == destinationProjectAndDoneIndex &&
-        source.index < destination.index;
-
-      // TODO: simplify
-      // We get the taskId surrounding or "englobing" the destination position
-      let TaskEnglobingDestinationTaskIndex = destinationIsEndOfDraggable
-        ? sortedTasksPerProjectAndDone[destination.droppableId].length - 1
-        : sourceIsBeforeDestination
-        ? destination.index + 1
-        : destination.index - 1;
-
-      let TaskEnglobingDestinationTask =
-        TaskEnglobingDestinationTaskIndex >= 0 &&
-        TaskEnglobingDestinationTaskIndex < destinationProjectSortedTasks.length
-          ? destinationProjectSortedTasks[TaskEnglobingDestinationTaskIndex]
-          : destinationProjectSortedTasks[destination.index]; // If this is the first or last task, we consider englobing task to be = destination task
-
-      // We deduce the priority to which we must assign the task, we try to preserve original priority if possible
-      let destinationPriority = sourceTask.priority;
-      if (
-        TaskEnglobingDestinationTask.priority !== sourceTask.priority &&
-        destinationTask.priority !== sourceTask.priority
-      ) {
-        destinationPriority = destinationTask.priority;
-      }
-
-      // We identify the "bucket" of cachedTasks where the task is going, and then we map the initials ids to the new bucket
-      let destinationProjectPriorityBucketIdStart =
-        destinationProjectSortedTasks.findIndex(
-          (task) => task.priority == destinationPriority
-        );
-
-      let inBucketDestinationIndex =
-        destination.index - destinationProjectPriorityBucketIdStart;
-
-      let destinationBucket = destinationProjectSortedTasks.filter(
-        (task) => task.priority == destinationPriority
-      );
-
-      let orders = destinationBucket.map((task) => task.order);
-      let newOrder = calcNewOrder(
-        sourceIsBeforeDestination,
-        inBucketDestinationIndex,
-        orders
-      );
-
-      // CUSTOM
-
-      // We create the task update instruction with only the required valeus
-      let taskUpdate: Required<Pick<TaskType, "id">> & Partial<TaskType> = {
-        id: sourceTask.id,
-        order: newOrder,
-      };
-
-      // If task is moving from not done to the done bucket, we don't update project and priority (but we do if moving from done to done)
-      if (source.droppableId !== "done" && destination.droppableId === "done") {
-        taskUpdate = { ...taskUpdate, done_at: dayjs().toString() };
-      } else if (
-        source.droppableId === "done" &&
-        destination.droppableId !== "done"
-      ) {
-        taskUpdate = { ...taskUpdate, done_at: null };
-      }
-
-      let destinationProjectId =
-        destination.droppableId !== "done"
-          ? destination.droppableId
-          : destinationTask.projectId; // TODO: use calculated destinationProjectId like for priority
-      if (destinationProjectId !== sourceTask.projectId) {
-        taskUpdate = { ...taskUpdate, projectId: destinationProjectId };
-      }
-
-      if (destinationPriority !== sourceTask.priority) {
-        taskUpdate = { ...taskUpdate, priority: destinationPriority };
-      }
-
-      // We update the task in cache in order to prevent blinking during the time when replicache update its state
-      setTasks(
-        tasks.map((task) =>
-          task.id === sourceTask.id ? { ...sourceTask, ...taskUpdate } : task
-        )
-      );
-
-      rep.mutate.taskUpdate(taskUpdate);
     },
     [sortedTasksPerProjectAndDone, tasks, rep]
   );
@@ -235,7 +166,7 @@ export function TaskList({ tasks: uncachedTasks }: { tasks: TaskType[] }) {
   return (
     <div
       className={classNames("pt-4", {
-        "border-b border-gray-200": tasks.length > 0,
+        "border-b border-gray-200 mb-32": tasks.length > 0,
       })}
     >
       <DragDropContext onDragEnd={onDragTask}>
--- src/spa/utils/Orderring.tsx
+++ src/spa/utils/Orderring.tsx
@@ -1,9 +1,11 @@
+import { DraggableLocation } from "@hello-pangea/dnd";
+
 export function calcNewOrder(
-  sourceIsBeforeDestination: boolean,
+  sourceIsMovingDestinationDown: boolean,
   destinationId: number,
   orders: number[]
 ) {
-  if (sourceIsBeforeDestination) destinationId += 1;
+  if (sourceIsMovingDestinationDown) destinationId += 1;
 
   let prevItemOrder = destinationId == 0 ? 0 : orders[destinationId - 1];
 
@@ -15,4 +17,131 @@ export function calcNewOrder(
   let newOrder = (prevItemOrder + nextItemOrder) / 2;
 
   return newOrder;
+}
+
+// This function help defining the new order and new subvalues of an item being moved in a DraggableArea.
+// The use case is for when you have multiple list (for ex.: categories) of the same type of items, and that
+// inside a single list, those items are themselves sorted by some criteria (subvalues), then by an order number
+// dragAndDropGeneric will do the all the hard calculations and call an itemUpdater with the new order and subvalues
+export function dragAndDropGeneric<T extends { order: number }>(
+  {
+    source,
+    destination,
+  }: {
+    source: DraggableLocation;
+    destination: DraggableLocation | null;
+  },
+  droppablesData: { [key: string]: T[] },
+  subValueAccessors: { [key: string]: (arg0: T) => any },
+  itemUpdater: (
+    source: DraggableLocation,
+    destination: DraggableLocation,
+    sourceItem: T,
+    newOrder: number,
+    newSubvalues: { [key: string]: any }
+  ) => void
+) {
+  if (
+    !destination ||
+    (destination.droppableId == source.droppableId &&
+      destination.index == source.index)
+  ) {
+    return;
+  }
+
+  let sourceItem = droppablesData[source.droppableId][source.index];
+
+  // If we move a task to the last position of a perProjectAndDone, the destination task will not exist, so we use the last task as a reference
+  let destinationIsEndOfDroppable =
+    destination.index >= droppablesData[destination.droppableId].length;
+
+  // We get destination item reference for subvalues infos, we use last item of dropablesData as reference is the new position is after the end
+  let destinationItem = destinationIsEndOfDroppable
+    ? droppablesData[destination.droppableId][
+        droppablesData[destination.droppableId].length - 1
+      ]
+    : droppablesData[destination.droppableId][destination.index];
+
+  // If source item is in same bucket as destination, and if source if before destination, then the destination item will move up in the list after the reorder, we have to take it into account
+  let sourceIsMovingDestinationDown =
+    source.droppableId == destination.droppableId &&
+    source.index < destination.index;
+
+  // We get the taskId surrounding or "englobing" the destination position, so we can ensure later that we only update source sub-values if they are different from both
+  let itemEnglobingDestinationIndex = destinationIsEndOfDroppable
+    ? droppablesData[destination.droppableId].length - 1
+    : sourceIsMovingDestinationDown
+    ? destination.index + 1
+    : destination.index - 1;
+
+  // If this is the first or last item of dropableData, we consider englobing task to be = destination task
+  if (
+    itemEnglobingDestinationIndex <= 0 ||
+    itemEnglobingDestinationIndex >=
+      droppablesData[destination.droppableId].length
+  ) {
+    itemEnglobingDestinationIndex = destination.index;
+  }
+
+  let itemEnglobingDestination =
+    droppablesData[destination.droppableId][itemEnglobingDestinationIndex];
+
+  // Calculating new subvalues
+  let newSubValues: { [key: string]: any } = {};
+
+  Object.keys(subValueAccessors).forEach((subValueKey) => {
+    let getValue = subValueAccessors[subValueKey];
+
+    // We deduce the subvalue to which we must assign the task, we try to preserve original subvalue if possible
+    newSubValues[subValueKey] = getValue(sourceItem);
+    if (
+      getValue(itemEnglobingDestination) !== getValue(sourceItem) &&
+      getValue(destinationItem) !== getValue(sourceItem)
+    ) {
+      // If we change value, we always get the upper value
+      newSubValues[subValueKey] = getValue(
+        sourceIsMovingDestinationDown
+          ? destinationItem
+          : itemEnglobingDestination
+      );
+    }
+  });
+
+  const areSubValuesEquals = (
+    item: T,
+    subvalues: { [key: string]: any }
+  ): boolean => {
+    let allEquals = true;
+
+    Object.keys(subvalues).forEach((key) => {
+      let getValue = subValueAccessors[key];
+
+      if (subvalues[key] !== getValue(item)) {
+        allEquals = false;
+      }
+    });
+
+    return allEquals;
+  };
+
+  // Bucket = Part of the droppableData that share the same subValues
+  // We identify the start of the bucket of items where the item is moving
+  let destinationBucketIdStart = droppablesData[
+    destination.droppableId
+  ].findIndex((item) => areSubValuesEquals(item, newSubValues));
+
+  let inBucketDestinationIndex = destination.index - destinationBucketIdStart;
+
+  let destinationBucket = droppablesData[destination.droppableId].filter(
+    (item) => areSubValuesEquals(item, newSubValues)
+  );
+
+  let bucketCurrentOrders = destinationBucket.map((item) => item.order);
+  let newOrder = calcNewOrder(
+    sourceIsMovingDestinationDown,
+    inBucketDestinationIndex,
+    bucketCurrentOrders
+  );
+
+  itemUpdater(source, destination, sourceItem, newOrder, newSubValues);
 }
