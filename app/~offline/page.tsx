091380fc256c2364a9e021c681d93f692c34cb82
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-21 18:04:54 +0200

    Auto redirect on offline

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,11 +1,21 @@
+"use client";
+
+import { redirect, usePathname } from "next/navigation";
+
 export default function OfflinePage() {
+  const pathname = usePathname();
+
+  if (pathname !== "/a") {
+    redirect("/a");
+  }
+
   return (
-    <div>
-      <p>You are offline and this page is not accessible through cache.</p>
+    <div className="flex flex-col items-center justify-center h-screen v-screen text-center gap-2">
+      <p>You are offline and Todobeast app isn't available in your cache.</p>
 
       {/* Use a instead of Link because next-pwa does support it on offline page */}
-      <a href="/a" className="text-blue-500 underline">
-        Click here to try loading the app from cache.
+      <a href="/a/planner" className="text-blue-500 underline">
+        Click here to reload.
       </a>
     </div>
   );
