1ab0aa8a12603e929731c0dc2a6ca50bc58b1782
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-21 19:08:47 +0200

    Well, custom fallback doesn't work, so using a tricky trick

---
+++ app/~offline/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const TodobeastApp = dynamic(() => import("../../src/spa/App"), {
+  ssr: false,
+});
+
+export default function TodobeastPage() {
+  return <TodobeastApp />;
+}
--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dynamicStartUrl: false,
   skipWaiting: false,
   scope: "/a",
-  fallbacks: {
-    //image: '/static/images/fallback.png',
-    document: "/a",
-    // font: '/static/font/fallback.woff2',
-    // audio: ...,
-    // video: ...,
-  },
 });
 
 module.exports = withPWA({});
