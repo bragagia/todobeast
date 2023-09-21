0463dd21b378e67485b0e20fe7f0c0fb28494c09
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-21 19:01:55 +0200

    Fallback on app instead of offline page

--- next.config.js
+++ next.config.js
@@ -2,8 +2,15 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   dynamicStartUrl: false,
-  cacheOnFrontEndNav: true,
+  skipWaiting: false,
   scope: "/a",
+  fallbacks: {
+    //image: '/static/images/fallback.png',
+    document: "/a",
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
 });
 
 module.exports = withPWA({});
