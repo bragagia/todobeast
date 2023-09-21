b689a1cc26808359f514a32c310fcb7506e62809
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-21 17:48:56 +0200

    Fix offline page

--- app/~offline/page.tsx
+++ app/~offline/page.tsx
@@ -1,13 +1,12 @@
-import { Link } from "react-router-dom";
-
 export default function OfflinePage() {
   return (
     <div>
       <p>You are offline and this page is not accessible through cache.</p>
 
-      <Link to="/a" className="text-blue-500 underline">
+      {/* Use a instead of Link because next-pwa does support it on offline page */}
+      <a href="/a" className="text-blue-500 underline">
         Click here to try loading the app from cache.
-      </Link>
+      </a>
     </div>
   );
 }
