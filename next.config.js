dd94d2410d5facb0ddf17ff1c7e9e9bdf39a7794
Mathias Bragagia <mathias.bragagia@epitech.eu>
2023-09-21 15:11:15 +0200

    Try to fix service worker

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
+const runtimeCaching = require("next-pwa/cache");
+
 const withPWA = require("next-pwa")({
   dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
 });
 
 module.exports = withPWA({
